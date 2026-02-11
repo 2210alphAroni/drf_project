89ad928940c1061f1b9b7d5c6027e54711328c91
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-11 22:14:04 +0600

    add new model field created_at

--- drf_first_project/StudentInfo/models.py
+++ drf_first_project/StudentInfo/models.py
@@ -6,6 +6,8 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
 
     def __str__(self):
         return self.name
