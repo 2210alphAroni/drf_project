df9a0e364040827d6f5194d0dbdd17c23c18d73e
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-02-13 22:42:42 +0600

    Create Post API for Students

--- drf_first_project/StudentInfo/models.py
+++ drf_first_project/StudentInfo/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
 
--- drf_first_project/StudentInfo/views.py
+++ drf_first_project/StudentInfo/views.py
@@ -12,3 +12,11 @@ class StudentListAPIView(APIView):
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
